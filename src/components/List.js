ccdf5cfbc0aa238232cefb04737a8b37f1d8e0c6
sanjeevjayasurya <sanjeev.jayasurya@zopsmart.com>
2020-12-02 01:11:10 +0530

    Add github user and list component

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function List({ data = [], renderItem, renderEmpty }) {
+    return !data.length ? (
+        renderEmpty
+    ) : (<ul>
+        {data.map((item, i) => (
+            <li key={i}>{renderItem(item)}</li>
+        ))}
+    </ul>);
+}
